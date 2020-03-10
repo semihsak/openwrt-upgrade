d6f41ca31ad89e27ec0cde4ad5ad33b3e7048b59
semih <semihsak@gmail.com>
2020-03-10 17:20:15 +0300

    first commit

--- opkg-upgrade.sh
+++ opkg-upgrade.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 SUCCESS=10;
-FAIL=20;
+NO=20;
+
 logger -s "Update package list"
 opkg update
-if (echo $? == 0)
-then
-	if ($(opkg list-upgradable | wc -l) != 0)
-	then
-        logger -s "Upgrade is starting";
-        name=$(opkg list-upgradable | cut -d' ' -f1);
+while [ $? -eq 0 ]
+do
+    if ($(opkg list-upgradable | wc -l) != 0)
+    then
+        logger -s "Upgrade is starting"
+        name=$(opkg list-upgradable | cut -d' ' -f1)
         opkg upgrade $name;
-        logger -s "Deleting -opkg file extends";
-        rm $(find / -name *-opkg);
-        logger -s "Services restarting";
-          	for names in $name
-          	do
-              /etc/init.d/${names} restart;
-          	done;
-      	exit $SUCCESS;
-    	fi
-logger -s "Not necessary upgrade packages";
-exit $FAIL;
-fi
+        logger -s "Deleting -opkg file extends"
+        rm $(find / -name *-opkg)
+        logger -s "Service Restarting"
+        for names in $name
+        do
+           /etc/init.d/${names} restart;
+        done
+        exit $SUCCESS
+    fi
+        echo "No upgradable packages"
+        exit $NO
+done
+
+echo "Check your network settings or internet connectivity"
