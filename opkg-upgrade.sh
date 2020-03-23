f7bf5ca6a25dee560877d691ec31b0f0489b2e76
Semih Sak <56431508+semihsak@users.noreply.github.com>
2020-03-23 21:03:39 +0300

    Update opkg-upgrade.sh

--- opkg-upgrade.sh
+++ opkg-upgrade.sh
@@ -6,22 +6,23 @@ logger -s "Update package list"
 opkg update
 while [ $? -eq 0 ]
 do
-    if ( echo $(opkg list-upgradable | wc -l) != 0)
-    then
-        logger -s "Upgrade is starting"
-        name=$(opkg list-upgradable | cut -d' ' -f1)
-        opkg upgrade $name;
-        logger -s "Deleting -opkg file extends"
-        rm $(find / -name *-opkg)
-        logger -s "Service Restarting"
-        for names in $name
-        do
-           /etc/init.d/${names} restart;
-        done
-        exit $SUCCESS
-    fi
-        logger -s "No upgradable packages"
-        exit $NO
+  if ( echo $(opkg list-upgradable | wc -l) != 0)
+  then
+      logger -s "Upgrade is starting"
+      name=$(opkg list-upgradable | cut -d' ' -f1)
+      opkg upgrade $name;
+      logger -s "Deleting -opkg file extends"
+      rm $(find / -name *-opkg)
+      logger -s "Service Restarting"
+      for names in $name
+      do
+         /etc/init.d/${names} restart;
+      done
+      exit $SUCCESS
+  else
+      logger -s "No upgradable packages"
+      exit $NO
+  fi
 done
 
 logger -s "Check your network settings or internet connectivity"
